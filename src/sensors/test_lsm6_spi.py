7b16b4758402962f4f1621dec0c15765d2511afc
Xiaorui Gu <xiaoruigu@dhcp-10-105-7-94.wireless.northwestern.private>
2026-07-07 10:43:26 -0500

    modified test_lsm6_spi.py to scanning all four devices

--- src/sensors/test_lsm6_spi.py
+++ src/sensors/test_lsm6_spi.py
@@ -1,11 +1,16 @@
 import spidev
 import time
 
-spi = spidev.SpiDev()
-spi.open(1, 0)          # test /dev/spidev0.0 first
-spi.max_speed_hz = 100000
-spi.mode = 0
+for bus, dev in [(0,0), (0,1), (1,0), (1,1)]:
+    print("Testing", bus, dev)
+    spi = spidev.SpiDev()
+    spi.open(bus, dev)
+    spi.max_speed_hz = 100000
+    spi.mode = 0
 
-while True:
-    rx = spi.xfer2([0xAA, 0x55])
-    time.sleep(0.2)
+    t0 = time.time()
+    while time.time() - t0 < 10:
+        spi.xfer2([0xAA, 0x55, 0xAA, 0x55])
+        time.sleep(0.05)
+
+    spi.close()
