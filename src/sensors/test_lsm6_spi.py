40b6040bf2c5e2b17d346a17ee8fc69d3116c907
Xiaorui Gu <xiaoruigu@dhcp-10-105-7-94.wireless.northwestern.private>
2026-07-07 10:34:39 -0500

    modifed testing lsm6 spi code

--- src/sensors/test_lsm6_spi.py
+++ src/sensors/test_lsm6_spi.py
@@ -2,10 +2,10 @@ import spidev
 import time
 
 spi = spidev.SpiDev()
-spi.open(0,1)
+spi.open(0, 0)          # test /dev/spidev0.0 first
 spi.max_speed_hz = 100000
-spi.mode = 3
+spi.mode = 0
 
 while True:
-    spi.xfer2([0xAA])
-    time.sleep(0.5)
+    rx = spi.xfer2([0xAA, 0x55])
+    time.sleep(0.2)
