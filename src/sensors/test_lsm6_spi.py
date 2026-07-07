cf5fcc4192b4f405c7a03ee81d45faf88839c860
Xiaorui Gu <xiaoruigu@dhcp-10-105-7-94.wireless.northwestern.private>
2026-07-07 09:54:59 -0500

    changed test_lsm6_spi.py

--- src/sensors/test_lsm6_spi.py
+++ src/sensors/test_lsm6_spi.py
@@ -1,23 +1,11 @@
-import spidev, time
-import Jetson.GPIO as GPIO
-
-CS_PIN = 22
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(CS_PIN, GPIO.OUT, initial=GPIO.HIGH)
+import spidev
+import time
 
 spi = spidev.SpiDev()
-spi.open(0, 1)
-spi.no_cs = True
-spi.mode = 0
+spi.open(0,0)
 spi.max_speed_hz = 100000
+spi.mode = 3
 
-GPIO.output(CS_PIN, GPIO.LOW)
-time.sleep(0.001)
-resp = spi.xfer2([0x8F, 0x00])
-GPIO.output(CS_PIN, GPIO.HIGH)
-
-print(resp)
-
-spi.close()
-GPIO.cleanup()
+while True:
+    spi.xfer2([0xAA])
+    time.sleep(0.5)
